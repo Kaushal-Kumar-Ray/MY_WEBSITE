2d8d667a597fbf49bf33f3eaac0e5d0b4ba8be54
Kaushal-Kumar-Ray <raykaushal456@gmail.com>
2025-03-23 11:22:06 +0530

    Picture gallery update

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,132 +1,112 @@
-const start_btn = document.querySelector(".start_quiz");
-const quiz_box = document.querySelector(".quiz-box");
-const que_text = quiz_box.querySelector(".que_text");
-const options_box = quiz_box.querySelector(".options");
-const next_btn = document.querySelector(".next-btn");
-const total_q = document.querySelector(".quiz-footer .total_que");
-const count_que = document.querySelector(".quiz-footer .count_que");
-const result_box = document.querySelector(".result-box");
+const startButton = document.querySelector(".start_quiz");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const questionText = document.getElementById("question");
+const optionsBox = document.querySelector(".options");
+const nextButton = document.querySelector(".next-btn");
+const scoreText = document.getElementById("score");
+const countQue = document.querySelector(".count_que");
+const totalQue = document.querySelector(".total_que");
+const playAgainButton = document.querySelector(".again-quiz");
+const exitButton = document.querySelector(".exit");
 
-const total_que_r = document.querySelector(".total-que span");
-const right_ans_r = document.querySelector(".right-ans span");
-const wrong_ans_r = document.querySelector(".wrong-ans span");
-const percentage = document.querySelector(".percentage span");
-
-const again_quiz = document.querySelector(".result-footer .again-quiz");
-const exit = document.querySelector(".result-footer .exit");
-
-const mark_wrong = '<i class="fa fa-times"></i>';
-const mark_check = '<i class="fa fa-check"></i>';
-
-
-
-start_btn.onclick =()=>{
-    quiz_box.classList.remove("inactive");
-    start_btn.classList.add("inactive");
-}
-
-total_q.innerText = questions.length;
-total_que_r.innerText = questions.length;
-
-var que_index = 0;
-var right_answers = 0;
-var wrong_answers = 0;
-count_que.innerText = que_index+1;
-ShowQuestion(que_index);
-
-function ShowQuestion(q_index){
-    que_text.innerText = questions[q_index].num+". "+ questions[q_index].question;
-var option_statement = "";
-for(var i=0; i<questions[q_index].options.length; i++){
-    option_statement += `<div class="option">${questions[q_index].options[i]}</div>`;
-}
-
-options_box.innerHTML = option_statement;
-
-var AllOptions = options_box.querySelectorAll(".option");
-
-    for(var j=0; j<AllOptions.length; j++){
-        AllOptions[j].setAttribute("onclick","UserAnswer(this)");
+const questions = [
+    {
+        question: "Which of the following is an example of DML?",
+        answer: "UPDATE",
+        options: ["CREATE", "UPDATE", "DROP", "ALTER"]
+    },
+    {
+        question: "Network that covers large areas is?",
+        answer: "MAN",
+        options: ["LAN", "MAN", "WAN", "None"]
+    },
+    {
+        question: "What is the output of 10/0 in JavaScript?",
+        answer: "Infinity is printed",
+        options: ["Nothing is printed", "0 is printed", "Infinity is printed", "Some Garbage Value"]
+    },
+    {
+        question: "FILE is of which type in C?",
+        answer: "struct type",
+        options: ["int type", "char * type", "struct type", "None of them"]
+    },
+    {
+        question: "Which OOP principle hides unnecessary details?",
+        answer: "Abstraction",
+        options: ["Abstraction", "Polymorphism", "Inheritance", "Encapsulation"]
     }
-    next_btn.classList.add("inactive");
-}
+];
 
+let currentQuestion = 0;
+let score = 0;
 
-next_btn.onclick=()=>{
-    que_index++;
-    
-    if(questions.length>que_index){
-        count_que.innerText = que_index+1;
-        ShowQuestion(que_index);
-    }else{
-        console.log("Questions Complete");
-        quiz_box.classList.add("inactive");
-        result_box.classList.remove("inactive");
-        right_ans_r.innerText = right_answers;
-        wrong_ans_r.innerText = wrong_answers;
-        percentage.innerText = ((right_answers*100)/questions.length).toFixed(2)+"%";
+totalQue.textContent = questions.length;
 
-        
+startButton.addEventListener("click", () => {
+    startButton.classList.add("inactive");
+    quizBox.classList.remove("inactive");
+    loadQuestion();
+});
+
+function loadQuestion() {
+    if (currentQuestion >= questions.length) {
+        showResults();
+        return;
     }
 
-    if(questions.length-1==que_index){
-        next_btn.innerText = "Finish";
-    }
+    let q = questions[currentQuestion];
+    questionText.textContent = q.question;
+    optionsBox.innerHTML = "";
+    countQue.textContent = currentQuestion + 1;
+
+    q.options.forEach(opt => {
+        let div = document.createElement("div");
+        div.classList.add("option");
+        div.textContent = opt;
+        div.onclick = () => checkAnswer(div, q.answer);
+        optionsBox.appendChild(div);
+    });
+
+    nextButton.classList.add("inactive");
 }
 
-function UserAnswer(answer){
-    let userAns = answer.innerText;
-    let correctAns = questions[que_index].answer;
-    var AllOptions2 = options_box.querySelectorAll(".option");
+function checkAnswer(selected, correct) {
+    let options = document.querySelectorAll(".option");
+    options.forEach(option => option.onclick = null);
 
-    next_btn.classList.remove("inactive");
-    if(userAns == correctAns){
-        console.log("%c Right Answer","color:green");
-        answer.classList.add("correct");
-        answer.insertAdjacentHTML("beforeend",mark_check);
-        right_answers++;
-    }else{
-
-        console.log("%c Wrong Answer","color:red");
-        answer.classList.add("incorrect");
-        answer.insertAdjacentHTML("beforeend",mark_wrong);
-        wrong_answers++;
-
-        for(var i=0; i<AllOptions2.length; i++){
-            if(AllOptions2[i].innerText==correctAns){
-                AllOptions2[i].classList.add("correct");
-                AllOptions2[i].insertAdjacentHTML("beforeend",mark_check);
-            }
-        }
-    }
-
-
-    for(var j=0; j<AllOptions2.length; j++){
-        AllOptions2[j].classList.add("disabled");
+    if (selected.textContent === correct) {
+        selected.classList.add("correct");
+        score++;
+    } else {
+        selected.classList.add("incorrect");
+        options.forEach(option => {
+            if (option.textContent === correct) option.classList.add("correct");
+        });
     }
 
+    nextButton.classList.remove("inactive");
 }
 
-again_quiz.onclick=()=>{
-    quiz_box.classList.remove("inactive");
-    result_box.classList.add("inactive");
+nextButton.addEventListener("click", () => {
+    currentQuestion++;
+    loadQuestion();
+});
 
-    reset();
-
+function showResults() {
+    quizBox.classList.add("inactive");
+    resultBox.classList.remove("inactive");
+    scoreText.textContent = `Your Score: ${score} / ${questions.length}`;
 }
 
-exit.onclick=()=>{
-    start_btn.classList.remove("inactive");
-    result_box.classList.add("inactive");
+playAgainButton.addEventListener("click", () => {
+    currentQuestion = 0;
+    score = 0;
+    resultBox.classList.add("inactive");
+    quizBox.classList.remove("inactive");
+    loadQuestion();
+});
 
-   reset();
-}
-
-function reset(){
-    que_index = 0;
-    right_answers = 0;
-    wrong_answers = 0;
-    next_btn.innerText = "Next Question";
-   count_que.innerText = que_index+1;
-   ShowQuestion(que_index);
-}
+exitButton.addEventListener("click", () => {
+    location.reload();
+});
